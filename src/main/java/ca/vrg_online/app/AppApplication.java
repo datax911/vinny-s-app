7ae894eb05d1a76c2023d3e506c0f6d640565557
Vinny <commutateur@gmail.com>
2025-08-09 14:55:19 -0400

    first commit

---
+++ src/main/java/ca/vrg_online/app/AppApplication.java
@@ -0,0 +1,13 @@
+package ca.vrg_online.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
